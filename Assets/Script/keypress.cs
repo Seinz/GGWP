5badfd589ced377ca260a6002e4c9a8b5041e5e8
Sein <miyoshi.asw@gmail.com>
2016-03-15 14:48:29 +0700

    First Commit

---
+++ Assets/Script/keypress.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class keypress : MonoBehaviour
+{
+    public Text outputText;
+    int counter = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            counter++;
+            outputText.text = "Key Press : " + counter;
+        }
+    }
+}
